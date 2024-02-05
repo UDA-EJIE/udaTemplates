9ced704f85ba47dfa6a5beddc7898c0b3896f734
hmartinez85 <34453480+hmartinez85@users.noreply.github.com>
2024-02-05 14:13:00 +0100

    Mejora en rutas

--- templates/plugin/paso2.py
+++ templates/plugin/paso2.py
@@ -11,11 +11,12 @@ def initPaso2(tables,yaml_data):
     dirController = directorio_actual+"controller/" 
     rutaWar = "src/com/ejie/"+proyectName+"/control" 
     war = proyectName+proyectWar+"War";
-    destinoControl = "C:/entorno/eclipseEsperanzaW11/eclipse202003EsperanzaW11/runtime-EclipseApplication/"+war+"/"+rutaWar 
+    destinoWar = yaml_data["destinoApp"]+war+"/" 
+    destinoWarControl = destinoWar+rutaWar
     # si no existe crear la carpeta, raiz control - config java
-    if os.path.isdir(destinoControl) == False:
-        os.mkdir(destinoControl)
-    with Worker(src_path=dirController, dst_path=destinoControl, data=yaml_data, exclude=["*troller*"]) as worker:
+    if os.path.isdir(destinoWarControl) == False:
+        os.mkdir(destinoWarControl)
+    with Worker(src_path=dirController, dst_path=destinoWarControl, data=yaml_data, exclude=["*troller*"]) as worker:
         worker.run_copy()
 
     #controller java   
@@ -29,6 +30,7 @@ data = { "project_name": "ppp",
         "security_app": "",
         "war_project_name": "Www",
         "PACKAGE_NAME": "com.ejie."+"ppp"+".control",
-        "directorio_actual" : "C:/aplic/x21aVersiones/4.x.x/udaTemplates/udaTemplates/templates/generateCode/"
+        "directorio_actual" : "C:/aplic/x21aVersiones/4.x.x/udaTemplates/udaTemplates/templates/generateCode/",
+        "destinoApp" : "C:/entorno/eclipseEsperanzaW11/eclipse202003EsperanzaW11/runtime-EclipseApplication/"
        }
 initPaso2(tables,data)    
