b326950b061eb10c39449a5a7a5ddae823ec7beb
Xabi <xaabi6@users.noreply.github.com>
2024-05-30 13:55:28 +0200

    Migrar plantillado de UDA 4 a UDA 6

--- proyecto/{{project_name}}Statics/WebContent/{{project_name}}/scripts/{{project_name}}{{war_project_name}}/mockLoginPage.js
+++ proyecto/{{project_name}}Statics/WebContent/{{project_name}}/scripts/{{project_name}}{{war_project_name}}/mockLoginPage.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2012 E.J.I.E., S.A.
+ * Copyright 2024 E.J.I.E., S.A.
  *
  * Licencia con arreglo a la EUPL, Versión 1.1 exclusivamente (la «Licencia»);
  * Solo podrá usarse esta obra si se respeta la Licencia.
@@ -14,52 +14,40 @@
  * que establece la Licencia.
  */
 
-jQuery(document).ready(function () {
-	
-	//Se Crea el combo de seleccion
-	$('#userCombo').rup_combo({
-		source : USERNAMES,
-		width: 250,
-		selected: "udaAnonymousUser",
-		select: function(event, object){
-			$('#selectedUserName').html(object.value);	
-		}
-	});
-	
-	$('#userCombo').rup_combo("setRupValue","udaAnonymousUser");
-	
-	//Se comprueba que hay feedback y si no se crea
-	if ($(".rup-feedback").size() === 0){
-		$("#content").prepend($("<div id='logFeedback'/>").addClass("logFeedback").rup_feedback({ 
-			type: "ok",
-			closeLink: true,
-			delay: 2500,
-			fadeSpeed: 500,
-			block: true
-		})).width($("#content").width());
-	} else {
-		$("#content").css("margin-top", "10em");
-	}
-	
-	//Se especifica el usuario pre-seleccionado
-	var udaMockUserName = $.rup_utils.readCookie("udaMockUserName");
-	if(udaMockUserName === null){
-		$('#selectedUser').append($("<span/>").addClass("selectedUserName").attr("id","selectedUserName").html($('#userCombo').rup_combo("getRupValue")));
-	} else {
-		$('#selectedUser').append($("<span/>").addClass("selectedUserName").attr("id","selectedUserName").html(udaMockUserName));
-		$('#userCombo').rup_combo("setRupValue",udaMockUserName);
-	}
-	
-	//Se especifica el funcionamiento del botón para que se envíen los datos y para que se almacene la cookie de usuario 
-	$('#loginButtonObject').bind("click",function(){
-		$.rup_utils.setCookie("udaMockUserName",$('#userCombo').rup_combo("getRupValue"),{duration : 0, path : "/"});
-		
-		if(MOCKURL !== undefined && MOCKURL !== null && MOCKURL !== ""){
-			$(location).attr('href',MOCKURL);
+jQuery(function($) {
+	const MOCKURL = $.rup_utils.getUrlVar('mockUrl');
+	const USERNAMES = $.parseJSON(($.rup_utils.getUrlVar('userNames')));
+
+	window.initRupI18nPromise.then(function() {
+		// Crea el select de seleccion.
+		$('#userCombo').rup_select({
+			data: USERNAMES,
+			allowClear: false,
+			selected: 'udaAnonymousUser'
+		});
+
+		// Comprueba que haya feedback y si no lo crea.
+		if ($('.rup-feedback').size() === 0) {
+			$('#content').prepend($('<div id=\'logFeedback\'/>').addClass('logFeedback').rup_feedback({
+				type: 'ok',
+				closeLink: true,
+				delay: 2500,
+				fadeSpeed: 500,
+				block: true
+			})).width($('#content').width());
 		} else {
-			$(".rup-feedback").rup_feedback("set",$.rup.i18nParse($.rup.i18n.app,"loginUserMock.logginOk1")+$('#userCombo').rup_combo("getRupValue")+$.rup.i18nParse($.rup.i18n.app,"loginUserMock.logginOk2")).feedback_principal.rup_feedback("show");
+			$('#content').css('margin-top', '10em');
 		}
-		
-		
+
+		// Especifica el funcionamiento del botón para que se envíen los datos y para que se almacene la cookie de usuario.
+		$('#loginButtonObject').bind('click', function() {
+			$.rup_utils.setCookie('udaMockUserName', $('#userCombo').rup_select('getRupValue'), { duration: 0, path: '/' });
+
+			if (MOCKURL !== undefined && MOCKURL !== null && MOCKURL !== '') {
+				$(location).attr('href', MOCKURL);
+			} else {
+				$('.rup-feedback').rup_feedback('set', $.rup.i18nParse($.rup.i18n.app, 'loginUserMock.logginOk1') + $('#userCombo').rup_select('getRupValue') + $.rup.i18nParse($.rup.i18n.app, 'loginUserMock.logginOk2')).feedback_principal.rup_feedback('show');
+			}
+		});
 	});
 });
